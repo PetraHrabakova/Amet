cc980cbebe2e57afbb984192a202a2044965e13d
Petra Hrabakova <64130090+PetraHrabakova@users.noreply.github.com>
2020-12-15 13:57:26 +0100

    Add files via upload

---
+++ GPS.ino
@@ -0,0 +1,36 @@
+void GPSread()
+{
+  uint8_t GPSchar;
+
+  // Serial.println(F("Wait for updated GPS Fix"));
+  GPSstarttimemS = millis();
+  
+  while (1)
+  {
+    if (gpsSerial.available() > 0)
+    {
+      GPSchar = gpsSerial.read();
+      gps.encode(GPSchar);
+    }
+
+    if (gps.location.isUpdated() && gps.altitude.isUpdated())
+    {
+      GPSendtimemS = millis();
+      break;
+    }
+  }
+}
+
+
+void GPSdisplay()
+{
+  GPSLat = String(gps.location.lat(), 6);
+  GPSLon = String(gps.location.lng(), 6);
+
+  Serial.print(F("Latitude: "));
+  Serial.println(GPSLat);
+  client.publish(latitude_topic, String(GPSLat).c_str());
+  Serial.print(F("Longitude: "));
+  Serial.println(GPSLon);
+  client.publish(longitude_topic, String(GPSLon).c_str());
+}
